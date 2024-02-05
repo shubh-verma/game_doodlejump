9ed51e5e63e8a302ff95151539ba8aab4e3ef263
shubh-verma <shubhvcs@gmail.com>
2024-02-05 15:02:37 +0530

    [Add] platform class

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let isGameOver = false;
   let doodlerBottomPosition = 150;
 
+  class Platform {
+    constructor(newPlatform) {
+      this.left = Math.random() * 315;
+      this.bottom = newPlatform;
+      this.visual = document.createElement("div");
+
+      const visual = this.visual;
+      visual.classList.add("platform");
+      visual.style.left = this.left + "px";
+      visual.style.bottom = this.bottom + "px";
+      grid.appendChild(visual);
+    }
+  }
+
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add("doodler");
