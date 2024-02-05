710a395feee2c5bde4015599ecad1b82643799ce
shubh-verma <shubhvcs@gmail.com>
2024-02-05 14:56:51 +0530

    [Add] doodler

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
+  const doodler = document.createElement("div");
+  let isGameOver = false;
+  let doodlerBottomPosition = 150;
+
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add("doodler");
+    doodler.style.bottom = doodlerBottomPosition + "px";
+  }
+  createDoodler();
 });
